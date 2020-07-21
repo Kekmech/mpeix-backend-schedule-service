8a963b33d3561bd13674ac38f12e2a91071cd48e
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2020-07-22 02:54:26 +0300

    fix sql request

--- src/main/kotlin/com/kekmech/SqlRequests.kt
+++ src/main/kotlin/com/kekmech/SqlRequests.kt
@@ -6,5 +6,5 @@ object SqlRequests {
 
     @Language("SQL")
     fun getGroupIdByGroupNumber(groupNumber: String) =
-        "select * from groups_info where number='$groupNumber' limit 1"
+        "select group_id from groups_info where group_number='$groupNumber' limit 1"
 }
