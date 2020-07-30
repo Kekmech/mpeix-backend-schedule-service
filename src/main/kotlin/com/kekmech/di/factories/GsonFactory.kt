fe68bdf4a8e00c7f0501d078feb0e5751c9ad346
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2020-07-31 02:21:34 +0300

    Bugfix

--- src/main/kotlin/com/kekmech/di/factories/GsonFactory.kt
+++ src/main/kotlin/com/kekmech/di/factories/GsonFactory.kt
@@ -9,6 +9,7 @@ object GsonFactory {
     fun create() = GsonBuilder().apply {
         setDateFormat(DateFormat.LONG)
         registerTypeAdapter(LocalDate::class.java, LocalDateSerializer())
+        registerTypeAdapter(LocalDate::class.java, LocalDateDeserializer())
         registerTypeAdapter(LocalTime::class.java, LocalTimeSerializer())
         registerTypeAdapter(LocalDateTime::class.java, LocalDateTimeSerializer())
     }.create()
