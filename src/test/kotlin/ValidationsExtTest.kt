02a4f094bf8f1210c750c251e7b3b3f1ff9022f5
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2020-09-12 20:05:00 +0300

    #0: fix test

--- src/test/kotlin/ValidationsExtTest.kt
+++ src/test/kotlin/ValidationsExtTest.kt
@@ -6,6 +6,6 @@ class ValidationsExtTest {
     @Test
     fun validateGroupNumberTest() {
         assert("С-6-18".checkIsValidGroupNumber() == "С-06-18")
-        assert("Э-1м-19".checkIsValidGroupNumber() == "Э-01м-19")
+        assert("Э-1м-19".checkIsValidGroupNumber() == "Э-01М-19")
     }
 }
