fb704db4d20cf7cc36e52cdd72baa93827b2c791
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2020-09-12 21:20:04 +0300

    #0: fix test again

--- src/main/kotlin/com/kekmech/ValidationsExt.kt
+++ src/main/kotlin/com/kekmech/ValidationsExt.kt
@@ -4,6 +4,6 @@ fun String?.checkIsValidGroupNumber(): String {
     if (!(this != null && matches("[а-яА-Я0-9-]{5,20}".toRegex())))
         throw ValidationException("Invalid group number")
     return this.toUpperCase().let {
-        if (it.matches(".*-\\d-.*".toRegex())) it.replaceFirst("-", "-0") else it
+        if (it.matches(".*-\\d[^0-9]*-.*".toRegex())) it.replaceFirst("-", "-0") else it
     }
 }
