f9bbc781272ecc4aab017712d4dfa16f308739bc
Mazunin Konstantin <mazuninky@gmail.com>
2020-12-02 23:39:40 +0300

    update ktor version and remove workaround about bug

--- src/main/kotlin/com/kekmech/schedule/controller/RestController.kt
+++ src/main/kotlin/com/kekmech/schedule/controller/RestController.kt
@@ -5,20 +5,10 @@ import com.kekmech.schedule.controller.rest.deprecated.getGroupSchedule
 import com.kekmech.schedule.controller.rest.v1.getGroupScheduleV1
 import io.ktor.application.*
 import io.ktor.locations.*
-import io.ktor.request.*
 import io.ktor.routing.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 fun Application.restModule() {
     install(Locations)
-
-    // Workaround for bug https://youtrack.jetbrains.com/issue/KTOR-1286
-    receivePipeline.intercept(ApplicationReceivePipeline.Before) {
-        withContext(Dispatchers.IO) {
-            proceed()
-        }
-    }
 
     routing {
         getGroupId()
